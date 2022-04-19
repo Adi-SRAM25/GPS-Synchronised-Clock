aad905a59a8c5769fe65ce8b2146514fbcdcfc5f
Aditya Sharma <87620220+Adi-SRAM25@users.noreply.github.com>
2022-04-19 18:01:55 +0530

    Update Counter_1.v

--- Counter_1.v
+++ Counter_1.v
@@ -5,20 +5,16 @@ module Counter_1 (reset,clk,q);
   input reset;
   output reg[27:0]q=28'd0;
   
-  reg [27:0] q1 = 28'd0;
-  reg sig_dly;
-  reg pe;
   
   always@(posedge clk , posedge reset)
     begin
-	 sig_dly <= reset;
+	 
      if(reset)
-       q1<=0;
+       q<=0;
      else
-       q1<=q1+1;
+       q<=q+1;
    end  
 	
- assign pe = reset & ~sig_dly; 
- assign q=(~pe)& q1;
+
   
 endmodule
